0691bc5fc253eb58eb5826fcfc7d2810c0b5d4b0
Urbanbrad <80424856+Urbanbrad@users.noreply.github.com>
2021-06-13 18:26:24 -0400

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.05,
+            length:10,
+
+        }
+        this.pointB=pointB
+        this.chain=Constraint.create(options)
+        World.add(world,this.chain)
+    }
+    fly(){
+        this.chain.bodyA=null
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        line(pointA.x,pointA.y,pointB.x,pointA.y);
+    }
+  }
+}
